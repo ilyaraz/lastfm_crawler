04209c65c0c1acff2f4cf6f431033bd7eff39257
Ilya Razenshteyn <ilyaraz@ilyaraz-debian.msu>
2012-05-13 15:37:38 +0400

    can crawl the graph

--- friends.py
+++ friends.py
@@ -15,10 +15,31 @@ def getFriendsPage(userName, page):
 
 def getFriendsList(userName):
     (numPages, friends) = getFriendsPage(userName, 1)
-    print numPages
     for i in xrange(2, numPages + 1):
         (_, newFriends) = getFriendsPage(userName, i)
         friends += newFriends
     return friends
 
-print getFriendsList(sys.argv[1])
+def crawlGraph(userName, depth):
+    print "depth 0"
+    print userName
+    users = [set([userName])]
+    for i in xrange(1, depth + 1):
+        users.append(set())
+        print "depth %d" % i
+        for user in users[i - 1]:
+            print "processing %s" % user
+            friends = getFriendsList(user)
+            for friend in friends:
+                new = True
+                for j in xrange(i + 1):
+                    if friend in users[j]:
+                        new = False
+                        break
+                if new:
+                    users[i].add(friend)
+    return users
+
+users = crawlGraph(sys.argv[1], int(sys.argv[2]))
+for (layerID, layer) in enumerate(users):
+    print "layer %d: %d users" % (layerID, len(layer))
